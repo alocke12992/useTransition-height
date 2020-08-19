418688b48d44600a18a723ad859182c61ac0abae
Andrew Locke <alocke12992@gmail.com>
2020-08-19 11:49:13 +0800

    Fix spring

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
 import useMeasure from 'react-use-measure';
-import './App.css';
 import { useTransition, animated } from 'react-spring'
 import styled from 'styled-components';
 
 const Animated = styled(animated.div)`
-  height: 0;
+  overflow: hidden;
   h1 {
     margin: 0;
   }
 `;
 
 function App() {
-  const [showText, setShowText] = useState(false);
+  const [animate, set] = useState(false);
   const [ref, { height }] = useMeasure()
-  const transition = useTransition(showText, {
-    from: { opacity: 0, overflow: 'hidden', height: 0 },
-    enter: { opacity: 1, height },
-    leave: { opacity: 0, height: 0 },
+  const transition = useTransition(animate, {
+    from: { height: 0 },
+    enter: { height },
+    leave: { height: 0 },
+    update: { height }
   });
-  console.log('height', height)
 
   return (
     <div className="App">
-      <button onClick={() => setShowText(!showText)}>Toggle Slide</button>
-      {transition((values) => (
+      <button onClick={() => set(!animate)}>Transition</button>
+      {transition((values, item) => item && (
         <Animated style={values}>
           <h1 ref={ref}>Hello</h1>
         </Animated>
